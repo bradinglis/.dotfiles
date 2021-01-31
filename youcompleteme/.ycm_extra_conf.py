fc1af5268b904ccfc702a08b20eeec6570c955bf
Brad Inglis <brad.inglis@gmail.com>
2021-01-31 14:43:15 +1100

    removed ycm (shit bloat software)

--- youcompleteme/.ycm_extra_conf.py
+++
@@ -1,8 +0,0 @@
-def FlagsForFile(filename, **kwargs):
-      flags = ['-Wall',
-              '-Wextra',
-              '-Werror',
-              '-x',
-              'c++'] # put your flag here
-      return { 'flags': flags }
-
